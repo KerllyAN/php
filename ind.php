db912a2042c7ce0c9ef08f976a5df230d891fa06
KerllyAN <73330369+KerllyAN@users.noreply.github.com>
2020-10-23 17:15:46 -0500

    h

---
+++ ind.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+<title>página de destino</title> 
+</head>
+<body>
+<h1>Al abrir esta página se han pasado las siguientes variables:</h1>
+<?php  
+$a=$_GET['a'];
+$b=$_GET['b'];
+$c=$_GET['c'];
+echo "<p>variable \$a : $a";
+echo "<p>variable \$b : $b";
+echo "<p>variable \$c : $c";
+?>
+</body>
+</html>
+
+
